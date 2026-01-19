1a8c9ea61e8ae41c8751c174fb78d79c9a41fc22
sxlanes <nicolaslopezsolanes@gmail.com>
2026-01-19 23:19:59 +0100

    Configurar GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import Demo253 from './pages/Demo253';
 
